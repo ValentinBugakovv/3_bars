fcbe658dfe9e4a6b91af3efb1c3709f26c2f8c80
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-10 18:35:43 +0300

    ver 0.1

--- bars.py
+++ bars.py
@@ -1,21 +1,78 @@
+import argparse
 import json
+from math import sqrt
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description="Path to json file"
+    )
+    parser.add_argument(
+        "filepath",
+        help="Path to json file",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def decorate(func):
+    def wrap(*args):
+        return json.dumps(func(args[0]),
+                          sort_keys=True,
+                          indent=4,
+                          ensure_ascii=False,
+                          separators=(",", ": ")
+                          )
+
+    return wrap
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r", encoding="UTF-8") as jsonfile:
+        return json.load(jsonfile)
 
 
-def get_biggest_bar(data):
-    pass
+@decorate
+def get_biggest_bar(bar_data):
+    biggest_bar = max(bar_data['features'], key=lambda bar:
+    bar['properties']['Attributes']['SeatsCount'])
+    attributes = biggest_bar["properties"]["Attributes"]
+    return attributes
 
 
-def get_smallest_bar(data):
-    pass
+@decorate
+def get_smallest_bar(bar_data):
+    smallest_bar = min(bar_data['features'], key=lambda bar:
+    bar['properties']['Attributes']['SeatsCount'])
+    attributes = smallest_bar["properties"]["Attributes"]
+    return attributes
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+@decorate
+def get_closest_bar(bar_data, longitude=37.55, latitude=55.75):
+    closest_bar = min(bar_data['features'], key=lambda bar: sqrt(
+        (bar['geometry']['coordinates'][0] - longitude) ** 2 +
+        (bar['geometry']['coordinates'][1] - latitude) ** 2))
+    attributes = closest_bar["properties"]["Attributes"]
+    return attributes
+
+
+def main():
+    longitude = float(input("Введите долготу:\n"))
+    latitude = float(input("Введите широту:\n"))
+    file_path = get_arguments().filepath
+    bar_data = load_data(file_path)
+    biggest_bar = get_biggest_bar(bar_data)
+    smallest_bar = get_smallest_bar(bar_data)
+    closest_bar = get_closest_bar(bar_data, longitude, latitude)
+    print(biggest_bar)
+    print(smallest_bar)
+    print(closest_bar)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        main()
+    except ValueError:
+        print("This is not a json file")
+
